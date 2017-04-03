f8c8ce450d5ec1de1dbccb39b611a277de7a3854
eduhg <edwinnyangena@gmail.com>
2017-04-03 10:51:01 +0300

    Add test command to manage.py

--- manage.py
+++ manage.py
@@ -36,5 +36,13 @@ def init_app():
     print "Migrations completed ........................................."
 
 
+@manager.command
+def test(coverage=False):
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
 if __name__ == '__main__':
     manager.run()
