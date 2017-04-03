b190f65792b88ef6f503fc110d7892492c2f33fe
eduhg <edwinnyangena@gmail.com>
2017-04-03 09:35:00 +0300

    Add full name to user model and run migrations

--- app/models.py
+++ app/models.py
@@ -6,4 +6,5 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(250), nullable=False)
+    full_name = db.Column(db.String(100), nullable=True)
     username = db.Column(db.String(100), nullable=False)
